a2cbfec9cdf678e91582bb9d887f1d7ba31c86a5
prof <prof@192.168.0.23>
2017-09-12 09:29:45 +0200

    first

--- cw2/cw2.php
+++ cw2/cw2.php
@@ -17,5 +17,27 @@
     var_dump($b);
     echo '<p>'.$a.' + '.$b. ' = '.($a+$b).'</p>';
 ?>
+<h3>Typ tekstowy</h3>
+<?php
+  $tekst = "ala ma kota";
+  var_dump($tekst);
+  echo '<br>'.$tekst[1].' długosć tekstu: '. strlen($tekst).'<br>';
+  for($i=0;$i<strlen($tekst);$i++){
+      echo $tekst[strlen($tekst)-1-$i];
+  }
+  echo '<br>';
+  for($i=0;$i<strlen($tekst);$i++){
+      echo $tekst[$i];
+      if($i< strlen($tekst)-1 
+              && $tekst[$i]!=' ' 
+              && $tekst[$i+1]!=' ') echo '-';
+  }
+  $tekst2 = "   Ala ma kociaka   ";
+  echo '<br>'.strlen($tekst2).'<br>';
+  echo strlen(trim($tekst2)).'<br>';
+  $long_text = 'ala ma kotka i ma tez jezyka';
+  $find = 'ma';
+  echo stripos($long_text, $find).'<br>';
+?>
 </body>
 </html>
