a83fdfa2fdf4c474dd945921a870e94f0508fae1
prof <prof@192.168.0.23>
2017-09-12 08:24:29 +0200

    first

---
+++ cw2/cw2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ćwiczenie 2</title>
+	<meta charset="utf-8">
+</head>
+<body>
+<h1>Typy liczbowe w php</h1>
+<?php
+	$a = 12;       //js: var a = 12;
+    var_dump($a);   //js: typeof(a);
+    echo "<br>";
+    echo '<h3>Fajny nagłówek h3</h3>';
+    echo 'Zmienna $a<br>';
+    echo "Zmienna $a<br>";
+    $b = 34.78;
+    var_dump($b);
+    echo '<p>'.$a.' + '.$b. ' = '.($a+$b).'</p>';
+?>
+</body>
+</html>
