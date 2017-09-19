cbbd4bdc236417437402b4e0dd6468f6ee3b2b77
prof <prof@192.168.0.23>
2017-09-19 10:26:02 +0200

    first

--- cw5/wynik.php
+++ cw5/wynik.php
@@ -18,7 +18,19 @@
             $plec = $_POST['plec'];
             $zaint = isset($_POST['zaint']) ? $_POST['zaint'] : [];
             if(trim($imie)!='' && trim($nazwisko)!=''){
-                
+              $powitanie = $plec=='K' ? "witamy kandydatkę w serwisie" 
+                   : "witamy kandydata w serwisie";
+              $zainteresowania = "Twoje zainteresowania: <ul>";
+              foreach ($zaint as $item) {
+                  $zainteresowania .= "<li>{$item}</li>";
+              }
+              $zainteresowania .= "</ul>";
+    echo <<< TEXT
+  <p> Dane zarejestrowanego kandydata </p>
+   <h3>{$imie} {$nazwisko} w klasie {$klasa}</h3>             
+   <p>{$powitanie}</p>  
+   <p>{$zainteresowania}</p>    
+TEXT;
             }else{
                  echo "<p>brak danych <a href='cw5.html'>Powrót do formularza</a></p>";
             }
