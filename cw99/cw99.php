e2a44610a8d4e0b26faefae8c293c920239d8800
prof <prof@192.168.0.23>
2017-09-26 08:24:34 +0200

    first

---
+++ cw99/cw99.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <style>
+            label{display: inline-block; width: 200px; text-align: right}
+            div.line{margin: 10px;}
+            input.zaint{margin-left: 210px;}
+            input#zat{width: 150px; padding: 5px;}
+        </style>
+    </head>
+    <body>
+        <div>
+            <form  method="post">
+                <fieldset>
+                    <legend>Wprowadź dane</legend>
+                    <div class="line">
+                        <label>Szerokość </label>
+                        <input type="text" name="width"/>
+                    </div>
+                    <div class="line">
+                        <label>Wysokość </label>
+                        <input type="text" name="height"/>
+                    </div>
+                     
+                   
+                    <div class="line">
+                        <label></label>
+                        <input type="submit" value="Generuj" id="zat"/>
+                    </div>
+                </fieldset>  
+            </form>
+        </div>
+        <div>
+            <?php
+            
+            ?>
+        </div>
+    </body>
+</html>
