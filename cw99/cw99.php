772033ad31eadeeda5d0e255ab5f746258a4b6c6
prof <prof@192.168.0.23>
2017-09-26 08:49:39 +0200

    first

--- cw99/cw99.php
+++ cw99/cw99.php
@@ -10,6 +10,11 @@
             div.line{margin: 10px;}
             input.zaint{margin-left: 210px;}
             input#zat{width: 150px; padding: 5px;}
+            table{border-collapse: collapse;}
+            td{padding: 5px; border: solid 1px brown; height: 20px; width: 20px;
+                text-align: center;}
+            td.first{color: #ffcccc; background-color: #ff6600;}
+            div#wynik{margin: 20px;}
         </style>
     </head>
     <body>
@@ -24,9 +29,7 @@
                     <div class="line">
                         <label>Wysokość </label>
                         <input type="text" name="height"/>
-                    </div>
-                     
-                   
+                    </div>   
                     <div class="line">
                         <label></label>
                         <input type="submit" value="Generuj" id="zat"/>
@@ -34,9 +37,24 @@
                 </fieldset>  
             </form>
         </div>
-        <div>
+        <div id="wynik">
             <?php
-            
+               if(isset($_POST['height']) && isset($_POST['width'])){
+                $width = intval($_POST['width']);
+                $height = intval($_POST['height']);
+                $height = $height<=0 || $height>40 ? 20 : $height;
+                $width = $width<=0 || $width>40 ? 20 : $width;
+                $html = '<table>';
+                for($i=1; $i<=$height;$i++){
+                    $html .= "<tr>";
+                    for($j=1; $j<=$width;$j++){
+                        $wyr = $i==1 || $j==1 ? " class='first' ":" ";
+                        $html .= "<td {$wyr}>".($i*$j)."</td>\n";
+                    }
+                    $html .= "</tr>\n";
+                }
+                echo $html.'</table>';
+            }
             ?>
         </div>
     </body>
