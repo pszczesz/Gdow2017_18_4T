b29c8b5f9e0d03ea5e3d200733f470e54037b8a0
prof <prof@192.168.0.2>
2017-11-10 09:31:25 +0100

    Merge origin/master
    Conflicts:
    	cw1/cw1.html
    	index.html

---
+++ egz1/salon.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Salon pielęgnacji</title>
+        <link href="salon.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <section id="baner"><h1>SALON PIELĘGNACJI PSÓW I KOTÓW</h1></section>
+        <section id="lewy">
+            <h3>
+                SALON ZAPRASZA W DNIACH
+            </h3>
+            <ul>
+                <li>Poniedziałek, 12:00 - 18:00</li>
+                <li>Wtorek, 12:00 - 18:00</li>
+            </ul>
+            <a href="pies.jpg"><img src="pies_mini.jpg" alt="pies"></a>
+            <p>Umów się telefonicznie na wizytę lub po prostu przyjdź!</p>
+        </section>
+        <section id="srodkowy">
+            <h3>PRZYPOMNIENIE O NASTĘPNEJ WIZYCIE</h3>
+        </section>
+        <section id="prawy">
+            <h3>USŁUGI</h3>
+        </section>
+        <?php
+        
+        ?>
+    </body>
+</html>
