a0d88d909dd5786742a632e8b33de02db424e73d
prof <prof@192.168.0.23>
2017-11-14 09:25:25 +0100

    ff2

--- egz1/salon.php
+++ egz1/salon.php
@@ -21,9 +21,44 @@
         </section>
         <section id="srodkowy">
             <h3>PRZYPOMNIENIE O NASTĘPNEJ WIZYCIE</h3>
+            <?php
+            $conn = new mysqli("localhost","root","","salon_prob");
+            if($conn->connect_errno>0){
+                echo "Błąd: ".$conn->connect_error;
+                exit();
+            }
+            $sql = "SELECT `imie`,`rodzaj`,`nastepna_wizyta`,`telefon`  "
+                    . "FROM `zwierzeta` where `nastepna_wizyta`<>0 ";
+            $result = $conn->query($sql);
+           // var_dump($result);           
+            if($result){
+                while($row = $result->fetch_assoc()){
+                   // print_r($row);
+                    $rodzaj = "";
+                    if($row['rodzaj']==1) {$rodzaj="Pies";}
+                    if($row['rodzaj']==2) {$rodzaj="Kot";}
+                    echo "<p>".$rodzaj." ".$row['imie']."</p>";
+                    echo "<p>Data następnej wizyty: {$row['nastepna_wizyta']},"
+                    . " telefon właściciela: {$row['telefon']}</p>";
+                }
+            }           
+            ?>
+            
         </section>
         <section id="prawy">
             <h3>USŁUGI </h3>
+            <?php
+            $sql =  "SELECT `nazwa`,`cena` FROM `uslugi` ";
+            $result=$conn->query($sql);
+            //print_r($result);
+            if($result){
+                while($row = $result->fetch_assoc()){
+                    echo "<p>{$row['nazwa']} {$row['cena']}</p>";
+                }
+            }
+            $conn->close();
+            ?>
+            <p><a href="dodaj.php">Dodaj nową usługę</a></p>
         </section>
         <?php
         
