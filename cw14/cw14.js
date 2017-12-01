1dfd775c8a326482b16bd357ac5be885d0b62e5c
prof <prof@192.168.0.2>
2017-12-01 10:25:16 +0100

    Merge origin/master
    Conflicts:
    	cw1/cw1.html
    	index.html

--- cw14/cw14.js
+++ cw14/cw14.js
@@ -1,7 +1,60 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+window.onload = function (){
+    document.getElementById("liczPole").onclick = function (){
+        var wybor = document.getElementsByName("figura");
+        console.log(wybor);
+        for(var i=0;i<wybor.length;i++){
+            if(wybor[i].checked) {
+                var wybWartosc = wybor[i].value;
+            }
+        }
+        console.log(wybWartosc);
+        switch(wybWartosc){
+            case "prostokat": ObliczProstokat();
+                break;
+             case "trojkat": ObliczTrojkat();
+                break; 
+            case "kolo": ObliczKolo();
+                break;
+            default : document.getElementById("wynik")
+                        .innerHTML = "BRAK DANYCH";
+        }
+        function ObliczProstokat(){
+            var a = parseFloat(document.getElementById("pa").value);
+            var b = parseFloat(document.getElementById("pb").value);
+            if(isNaN(a)|| isNaN(b)){
+                document.getElementById("wynik")
+                        .innerHTML = "Błędne dane!!";
+                return ;
+            }
+            document.getElementById("wynik")
+                    .innerHTML = "Pole prostokąta wynosi: "+(a*b).toFixed(2);
+            return ;
+        }
+        function ObliczTrojkat(){
+            var a = parseFloat(document.getElementById("ta").value);
+            var h = parseFloat(document.getElementById("th").value);
+            if(isNaN(a)|| isNaN(h)){
+                document.getElementById("wynik")
+                        .innerHTML = "Błędne dane!!";
+                return ;
+            }
+            document.getElementById("wynik")
+                    .innerHTML = "Pole trójkąta wynosi: "+(1/2*(a*h).toFixed(2));
+            return ;
+        }
+        function ObliczKolo(){
+            var r = parseFloat(document.getElementById("kr").value);
+            
+            if(isNaN(r)){
+                document.getElementById("wynik")
+                        .innerHTML = "Błędne dane!!";
+                return ;
+            }
+            document.getElementById("wynik")
+                    .innerHTML = "Pole koła wynosi: "+(Math.PI*r*r).toFixed(2);
+            return ;
+        }
+    };
+};
 
 
